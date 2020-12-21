6a7b73e8ee93e35bc46c89b51c98a90c60d5ae98
Andrii Kozachenko <andrew.kozachenko@gmail.com>
2020-12-21 14:37:09 +0200

    simple refactor

--- src/main/java/com/kozachenko/leson/lesson5/Main.java
+++ src/main/java/com/kozachenko/leson/lesson5/Main.java
@@ -2,6 +2,7 @@ package com.kozachenko.leson.lesson5;
 
 public class Main {
     public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
         double a = 10.88;
         int b = 3;
         Operation operation = OperationFactory.getOperation(a,b);
@@ -9,5 +10,7 @@ public class Main {
         System.out.println("Multiplication ==>" + operation.multiplication(a,b));
         System.out.println("Divide ==>"         + operation.divide(a,b));
         System.out.println("Difference ==>"     + operation.difference(a,b));
+        long runningTime = System.currentTimeMillis()-startTime;
+        System.out.println("Running time " + runningTime);
     }
 }
